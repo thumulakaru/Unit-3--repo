d0e89f19b1352726ae90586c3e4cb5b86a033be7
Thumula Karunaratne <111560418+thumulakaru@users.noreply.github.com>
2023-02-04 13:29:42 +0900

    Delete Quiz_33.py

--- Quizzes/Quiz_33.py
+++
@@ -1,7 +0,0 @@
-def mystery(list1:list, list2: list) -> list:
-    output = []
-    for l1_element in list1:
-        for l2_element in list2:
-            if l1_element == l2_element:
-                output.append(l1_element)
-    return output
